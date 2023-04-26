472d479ad7af797fa70a449b43617f3c3bcb5b3e
raimundosilvw <mengao-10r@hotmail.com>
2023-04-25 21:55:33 -0300

    Estilo global

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App(){
   return (
     <ThemeProvider theme={defaultTheme}>
       <Button varient="primary"/>
       <Button varient="secondary"/>
+
+      <GlobalStyle /> 
     </ThemeProvider>
   )
 }
